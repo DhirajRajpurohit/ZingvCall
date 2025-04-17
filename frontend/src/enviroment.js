804e1092f514bd93372665750d67618e461eae5b
Dhiraj Rajpurohit <rajpurohitdhiraj360@gmail.com>
2025-04-17 22:26:50 +0530

    Fixing the issue

--- frontend/src/enviroment.js
+++ frontend/src/enviroment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend-3xpg.onrender.com" :
+    "https://apnacollegebackend-ee2x.onrender.com" :
 
     "http://localhost:8000"
 
